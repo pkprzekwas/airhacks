dc47867807b10f8b8aa2eb334c5d0744d16947d9
Adam Bien <abien@adam-bien.com>
2017-05-17 17:09:30 +0200

    Classic bulkheads

--- press/src/main/java/pl/infoshare/press/media/boundary/MediaCoveragesResource.java
+++ press/src/main/java/pl/infoshare/press/media/boundary/MediaCoveragesResource.java
@@ -1,9 +1,15 @@
 
 package pl.infoshare.press.media.boundary;
 
+import static java.util.concurrent.CompletableFuture.supplyAsync;
+import java.util.concurrent.TimeUnit;
+import javax.annotation.Resource;
+import javax.enterprise.concurrent.ManagedExecutorService;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
 
 /**
  *
@@ -15,8 +21,16 @@ public class MediaCoveragesResource {
     @Inject
     MediaCoverageAgent agent;
 
+    @Resource
+    ManagedExecutorService mes;
+
     @GET
-    public String all() {
+    public void all(@Suspended AsyncResponse response) {
+        response.setTimeout(2, TimeUnit.SECONDS);
+        supplyAsync(this::getContent, mes).thenAccept(response::resume);
+    }
+
+    String getContent() {
         return "press: -> " + this.agent.all();
     }
 

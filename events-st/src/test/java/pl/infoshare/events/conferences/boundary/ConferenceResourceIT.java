bb3dfeb7fe0fdf8bb1e028222029aa93404c283f
Adam Bien <abien@adam-bien.com>
2017-05-17 14:56:54 +0200

    system test is working

---
+++ events-st/src/test/java/pl/infoshare/events/conferences/boundary/ConferenceResourceIT.java
@@ -0,0 +1,43 @@
+/*
+ */
+package pl.infoshare.events.conferences.boundary;
+
+import javax.json.JsonArray;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author airhacks.com
+ */
+public class ConferenceResourceIT {
+
+    private Client client;
+    private WebTarget tut;
+
+    @Before
+    public void initClient() {
+        this.client = ClientBuilder.newClient();
+        this.tut = this.client.target("http://localhost:8080/events/resources/conferences");
+    }
+
+    @Test
+    public void all() {
+        Response allResponse = this.tut.request().get();
+        assertThat(allResponse.getStatus(), is(200));
+        JsonArray conferences = allResponse.readEntity(JsonArray.class);
+        assertNotNull(conferences);
+        System.out.println("conferences = " + conferences);
+
+    }
+
+
+
+}
